452cdae474f97b637bcb50deeacd43254eb2c0f8
Jose A. Raposo <joseantonio.raposo.perez@iesdonana.org>
2024-02-06 21:03:55 +0100

    Empezando Actividad

---
+++ A01T7-JARaposo.js
@@ -0,0 +1,36 @@
+/* let respuestaURL = "./A01T7-JARaposo.json"
+const cliente = new XMLHttpRequest();
+
+cliente.open("GET", respuestaURL);
+cliente.responseType = "text";
+cliente.send();
+
+cliente.onload = function(){
+    const alumnosText = cliente.response;
+    const alumno = JSON.parse(alumnosText);
+    mostrar(alumno);
+    mostrar(alumnosText);
+} */
+alumnos();
+
+async function alumnos(){
+    const respuestaURL = "./A01T7-JARaposo.json"
+    const cliente = new Request(respuestaURL);
+
+    const respuesta = await fetch(cliente);
+    const alumnosText = await respuesta.text();
+
+    const alumnos = JSON.parse(alumnosText);
+    crear(alumnos);
+    mostrar(alumnosText);
+
+}
+
+function mostrar(al){
+    console.log(al);
+}
+
+function crear(al){
+    
+}
+
